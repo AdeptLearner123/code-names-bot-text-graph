a365e82988e002f85af525dec577956018045bb0
Nalu Zou <naluzou@Nalus-MacBook-Pro.local>
2022-10-21 15:34:43 -0700

    Update config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ SENSE_INVENTORY = "output/sense_inventory.json"
 TEXT_LIST = "output/text_list.txt"
 DICTIONARY_TEXT_LINKED = "output/dictionary_text_linked.json"
 
-SENSE_LABELS = "output/sense_labels.json"
-SYNONYM_SENSE_LABELS = "output/synonym_sense_labels.json"
+TEXT_SENSE_LABELS = "output/text_sense_labels.json"
+SEM_LINK_SENSE_LABELS = "output/sem_link_sense_labels.json"
 
 CONSEC_MODEL_STATE = "model_states/consec_semcor_normal_best_sense_extractor.pt"
